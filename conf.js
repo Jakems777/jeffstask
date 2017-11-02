02fac46f8d455e69d6f82ff6534366425e14084d
Jevgeni Musatov <Jevgeni.Musatov@myjar.com>
2017-11-02 15:48:04 +0200

    adding of conf.js

---
+++ conf.js
@@ -0,0 +1,10 @@
+/**
+ * Created by jevgenim on 02/11/2017.
+ */
+const conf = function () {
+    this.connStr = "postgres://jevgenim:12345678@localhost:5432/client";
+    this.appPort = '3000';
+    this.algorithm = 'aes-256-ctr';
+    this.password = 'd6F3Efeq';
+};
+module.exports = new conf();
